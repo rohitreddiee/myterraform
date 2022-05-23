60bd5a43410198f2a6e2477d0334691e1f481c30
Rohit Reddy <rohitreddy@Rohits-MacBook-Air.local>
2022-05-23 22:10:08 +0530

    hello this ia ram linga

---
+++ abc.tf
@@ -0,0 +1,26 @@
+{
+    "Parameters":{
+        "ImageId":{
+            "Type":"String",
+            "Description":"Enter Valid AMI"
+        }
+    },
+    "Resorces":{
+        "v1":{
+            "Type":"AWS::EC2::Volume",
+            "Properties":{
+                "Availabilityzone":"ap-southeast-1a",
+                "Size":{"Ref":"vsz"},
+                "VolumeType":"gp2"
+            }
+        }
+    },
+    "Outputs":{
+        "vszval":{
+            "Value":{"Ref":"vsz"}
+        },
+        "volid":{
+            "Value":{"Ref":"v1"}
+        }
+    }
+}
